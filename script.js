96aa4f329d6e869b10104a915ccc1bf688f0e391
Crislei <110057022+CRISRODRIM@users.noreply.github.com>
2022-08-11 16:35:50 -0300

    .

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
+// estado da aplicação
+
+const tarefas = []
+
+// alteradores de estado de aplicação
+
 function add(){
-    const ul = document.querySelector("ul")
-    const input = document.querySelector("input")
-    const tarefa = input.value
+const input = document.querySelector("input")
+const valor = input.value
+tarefas.push (valor)
+input.value = ""
+render()}
 
-    const li = document.createElement("li")
-    li.innerText = tarefa
-    ul.appendChild(li)
 
-    input.value = ""
-}
+// mostrar estado na tela
+function render(){
+
+const ul = document.querySelector("ul")
+ul.innerHTML = null
+
+tarefas.forEach(function(valor){
+
+const li = document.createElement("li")
+li.innerText = valor
+ul.appendChild(li)
+}) } 
+//iniciar
+render ()
